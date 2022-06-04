45315785d6dda084a7c6f253122cc45cbb320324
Jan-Åge Johnsen <jaj@skill.no>
2022-06-04 13:23:32 +0200

    update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Setting up Azure API Management with Dynamics 365 in 30 minutes",
+        "excerpt":"Dynamics 365 comes with a robust and powerful REST API out of the box. However, when integrating with other parties, I prefer to use Azure API Management (APIM) as an intermediary for a multitude of reasons. The setup used to be somewhat convoluted, but is now much simpler. This post...","categories": [],
+        "tags": ["APIM","Dynamics 365","Power Platform","Managed Identity"],
+        "url": "/2022/apim-and-crm-in-30-minutes/",
+        "teaser": null
+      }]
