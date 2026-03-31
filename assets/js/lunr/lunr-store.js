d10ee84dc64f39db0e2d896266a0f70ab7633102
Jan Åge Johnsen <jan.age.johnsen@gmail.com>
2026-03-31 18:17:41 +0000

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,16 @@ var store = [{
         "tags": ["APIM","Dynamics 365","Power Platform","Managed Identity"],
         "url": "/2022/apim-and-crm-in-30-minutes/",
         "teaser": null
+      },{
+        "title": "Migrating Azure DevOps Projects - Part 1: Repos and Wikis",
+        "excerpt":"Migrating Azure DevOps projects between organizations (hopefully) isn’t something you do every day, but when the need arises—whether due to a merger, reorganization, or other reasons—it can be a challenging process. Breaking the work down, you’re mostly dealing with Boards/Work Items, Repos, Wikis, Pipelines, Test Plans, and Artifacts. You’d expect...","categories": [],
+        "tags": ["Azure DevOps","Git","Migration","Wikis"],
+        "url": "/2025/migrating-azure-devops-projects-part-1-repos-and-wikis/",
+        "teaser": null
+      },{
+        "title": "Migrating Azure DevOps Projects - Part 2: Boards & Work Items",
+        "excerpt":"In Part 1, we covered how to migrate Repos and Wikis. Now we’ll look into the more complex challenge of migrating Boards and Work Items. This process is more demanding and requires additional tooling. Fortunately, azure-devops-migration-tools, created by Martin Hinshelwood, provides a way to move Work Items, Test Plans &amp;...","categories": [],
+        "tags": ["Azure DevOps","Migration","Boards","Work Items"],
+        "url": "/2025/migrating-azure-devops-projects-part-2-boards-and-work-items/",
+        "teaser": null
       }]
